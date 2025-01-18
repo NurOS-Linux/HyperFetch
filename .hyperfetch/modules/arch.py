5c11990f712f2ad2f7b1bb03940235e9ac935b68
AnmiTaliDev <anmitali198@gmail.com>
2025-01-19 01:01:40 +0500

    Обновил модуль

--- .hyperfetch/modules/arch.py
+++ .hyperfetch/modules/arch.py
@@ -7,16 +7,13 @@ def get_architecture_info():
     """
     architecture = platform.architecture()
     machine = platform.machine()
-    processor = platform.processor() or "Unknown Processor"
     
     return {
         'architecture': architecture,
         'machine': machine,
-        'processor': processor
     }
 
 def run():
     info = get_architecture_info()
     print(f'{Fore.GREEN}Architecture:{Style.RESET_ALL} {info["architecture"][0]}')
     print(f'{Fore.GREEN}Machine:{Style.RESET_ALL} {info["machine"]}')
-    print(f'{Fore.GREEN}Processor:{Style.RESET_ALL} {info["processor"]}')
